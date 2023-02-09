ea693ebdc12e4d35b81dfc1e5e91080f73bcafae
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-08 23:12:34 -0300

    state to change session status

--- TypeBank/src/components/AppContext.tsx
+++ TypeBank/src/components/AppContext.tsx
@@ -1,15 +1,17 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 interface IAppContext {
   user: string
   isLoggedIn: boolean
+  setIsLoggedIn: (isLoggedIn: boolean) => void
 }
 
 export const AppContext = createContext({} as IAppContext)
 export const AppContextProvider = ({ children }: any) => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
   const user = 'dilan'
-  const isLoggedIn = true
+
   return (
-    <AppContext.Provider value={{ user, isLoggedIn }}>
+    <AppContext.Provider value={{ user, isLoggedIn, setIsLoggedIn }}>
       {children}
     </AppContext.Provider>
   )

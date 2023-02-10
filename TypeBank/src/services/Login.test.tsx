cb9191e5856f565c10014405a7e2df6827d74029
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-09 21:18:26 -0300

    refactor test

--- TypeBank/src/services/Login.test.tsx
+++ TypeBank/src/services/Login.test.tsx
@@ -1,33 +1,18 @@
 import { login } from './login'
 
-const mocksetIsLoggedIn = jest.fn()
-const mockNavigate = jest.fn()
-
-jest.mock('react', () => ({
-  ...jest.requireActual('react'),
-  useContext: () => ({
-    setIsLoggedIn: mocksetIsLoggedIn
-  })
-}))
-
-jest.mock('react-router-dom', () => ({
-  ...(jest.requireActual('react-router-dom') as any),
-  useNavigate: () => mockNavigate
-}))
-
 describe('deve exibir alert com mensagem bem vindos', () => {
   const mockAlert = jest.fn()
   window.alert = mockAlert
 
   it('login', async () => {
-    await login('di@teste.com')
-    expect(mocksetIsLoggedIn).toHaveBeenCalledWith(true)
-    expect(mockNavigate).toHaveBeenCalledWith('/123')
+    const mockEmail = 'di@teste.com'
+    const response = await login(mockEmail)
+    expect(response).toBeTruthy()
   })
 
   it('deve exibir erro caso email seja invalido', async () => {
-    await login('invalido@email')
+    const response = await login('invalido@email')
 
-    expect(mockAlert).toHaveBeenCalledWith('Email invalido')
+    expect(response).toBeFalsy()
   })
 })

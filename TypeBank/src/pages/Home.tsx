c30ea42c4ae1121b0da4b9c48ac37fe5f1761735
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-09 21:34:19 -0300

    refactor login sistem

--- TypeBank/src/pages/Home.tsx
+++ TypeBank/src/pages/Home.tsx
@@ -1,11 +1,25 @@
 import { Form } from '../components/Form'
 import { Box, Center, Input } from '@chakra-ui/react'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { login } from '../services/login'
 import { ButtonContent } from '../components/Button'
+import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../components/AppContext'
 
 export default function Home() {
   const [email, setEmail] = useState('')
+  const navigate = useNavigate()
+  const { setIsLoggedIn } = useContext(AppContext)
+  const validadeUser = async (email: string) => {
+    const loggedIn = await login(email)
+
+    if (!loggedIn) {
+      alert('Email inválido')
+    } else {
+      setIsLoggedIn(true)
+      navigate('/conta/123')
+    }
+  }
   return (
     <Form>
       <Box backgroundColor="#FFFFFF" borderRadius="25px" padding="15px">
@@ -20,7 +34,7 @@ export default function Home() {
 
         <Input placeholder="password" />
         <Center>
-          <ButtonContent onClick={() => login(email)} title="Entrar" />
+          <ButtonContent onClick={() => validadeUser(email)} title="Entrar" />
         </Center>
       </Box>
     </Form>
--- TypeBank/src/services/Login.tsx
+++ TypeBank/src/services/Login.tsx
@@ -1,14 +1,9 @@
 import { api } from './api'
-import { AppContext } from '../components/AppContext'
-import { useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
-export const login = async (email: string): Promise<void> => {
-  const { setIsLoggedIn } = useContext(AppContext)
-  const navigate = useNavigate()
+
+export const login = async (email: string): Promise<boolean> => {
   const data: any = await api
   if (email !== data.email) {
-    return alert('Email invalido')
+    return false
   }
-  setIsLoggedIn(true)
-  navigate(`/${data.id}`)
+  return true
 }

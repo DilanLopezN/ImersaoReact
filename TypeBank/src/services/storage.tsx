310ba157457949316ed7f012b03396741a0f5460
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-18 20:11:02 -0300

    set storage test

--- TypeBank/src/services/storage.tsx
+++ TypeBank/src/services/storage.tsx
@@ -1,3 +1,11 @@
 export const getLocalStorage = () => {
   return localStorage.getItem('')
 }
+
+const typeBank = {
+  login: false
+}
+
+export const createLocalStorage = (): void => {
+  localStorage.setItem('typebank', JSON.stringify(typeBank))
+}

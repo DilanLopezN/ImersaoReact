a95474c65b7a9e88d84a9092da4aa68ee38a311d
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-18 20:15:13 -0300

    test set item

--- TypeBank/src/services/storage.test.tsx
+++ TypeBank/src/services/storage.test.tsx
@@ -1,9 +1,22 @@
-import { getLocalStorage } from './storage'
+import { json } from 'node:stream/consumers'
+import { createLocalStorage, getLocalStorage } from './storage'
 
+const typeBank = {
+  login: false
+}
 describe('storage', () => {
   const getItem = jest.spyOn(Storage.prototype, 'getItem')
   it('deve retornar objeto localStorage', () => {
     getLocalStorage()
     expect(getItem).toHaveBeenCalled()
+  })
+
+  it('deve criar objeto no localStorage', () => {
+    const mockSetItem = jest.spyOn(Storage.prototype, 'setItem')
+    createLocalStorage()
+    expect(mockSetItem).toHaveBeenCalledWith(
+      'typeBank',
+      JSON.stringify(typeBank)
+    )
   })
 })

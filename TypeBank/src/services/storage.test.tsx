75f99a04501aa4b65ff28c13ccfac991455c2dcf
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-19 15:46:15 -0300

    change localstorage

--- TypeBank/src/services/storage.test.tsx
+++ TypeBank/src/services/storage.test.tsx
@@ -1,10 +1,15 @@
 import { json } from 'node:stream/consumers'
-import { createLocalStorage, getLocalStorage } from './storage'
+import {
+  changeLocalStorage,
+  createLocalStorage,
+  getLocalStorage
+} from './storage'
 
 const typeBank = {
   login: false
 }
 describe('storage', () => {
+  const mockSetItem = jest.spyOn(Storage.prototype, 'setItem')
   it('deve retornar objeto localStorage', () => {
     const mockGetItem = jest.spyOn(Storage.prototype, 'typebank')
     getLocalStorage()
@@ -12,11 +17,15 @@ describe('storage', () => {
   })
 
   it('deve criar objeto no localStorage', () => {
-    const mockSetItem = jest.spyOn(Storage.prototype, 'setItem')
     createLocalStorage()
     expect(mockSetItem).toHaveBeenCalledWith(
       'typebank',
       JSON.stringify(typeBank)
     )
+  })
+
+  it('deve alterar o valor do objeto no localStorage', () => {
+    changeLocalStorage('exemple')
+    expect(mockSetItem).toHaveBeenCalledWith('typebank', 'example')
   })
 })
--- TypeBank/src/services/storage.tsx
+++ TypeBank/src/services/storage.tsx
@@ -1,11 +1,19 @@
-export const getLocalStorage = () => {
-  return localStorage.getItem('typebank')
+interface TypeBank {
+  login: boolean
 }
 
 const typeBank = {
   login: false
 }
 
+export const getLocalStorage = () => {
+  return localStorage.getItem('typebank')
+}
+
 export const createLocalStorage = (): void => {
   localStorage.setItem('typebank', JSON.stringify(typeBank))
+}
+
+export const changeLocalStorage = () => {
+  return 'value'
 }
